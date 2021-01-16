a491dce5a387feb992db021644ef44d05660d0d7
Gilberto <peter01@hotmail.es>
2021-01-16 12:06:39 -0500

    [Frontend] Finished styling the posts section

--- instagram_clone/src/components/UI/Posts/index.js
+++ instagram_clone/src/components/UI/Posts/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
+
 import { BsThreeDots, BsChatDots } from 'react-icons/bs';
 import { VscBookmark } from 'react-icons/vsc';
 import { IoIosHeartEmpty } from 'react-icons/io';
+
+import formatDistance from 'date-fns/formatDistance';
+import format from 'date-fns/format';
+import { es } from 'date-fns/locale';
 
 const MainContainer = styled.main`
   margin-top: 54px;
@@ -229,10 +234,49 @@ const PostFooterTitle = styled.div`
   }
 `;
 
+const PostFooterSeeComments = styled.div`
+  width: 100%;
+  padding: 0 1.6rem;
+  margin: 4px 0;
+
+  a {
+    display: block;
+    width: 100%;
+    font-size: 1.4rem;
+    text-decoration: none;
+    color: #8e8e8e;
+  }
+`;
+
+const PostFooterComments = styled.div`
+  width: 100%;
+  padding: 0 1.6rem;
+
+  span {
+    font-size: 1.4rem;
+    
+    &:first-of-type {
+      font-weight: bold;
+    }
+  }
+`;
+
+const PostFooterTimer = styled.div`
+  width: 100%;
+  padding: 0 1.6rem;
+  margin: 4px 0;
+
+  a {
+    text-decoration: none;
+    font-size: 1.4rem;
+    color: #8e8e8e;
+  }
+`;
+
 const Posts = () => {
 
-  const posts = [{ owner: 'Sussan_97', title: 'Exploracion urbana!', profilePhoto: 'https://images.unsplash.com/photo-1558898479-33c0057a5d12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', photoURL: 'https://images.unsplash.com/photo-1602525962574-3bc829fbed3c?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' },
-  { owner: 'Paula_sofi03', title: 'Enamorados el uno del otro!', profilePhoto: 'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', photoURL: 'https://images.unsplash.com/photo-1610141095202-4f36dc1e7899?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=645&q=80' }];
+  const posts = [{ owner: 'Sussan_97', title: 'Exploracion urbana!', createdAt: '2021-01-5 14:33:09.478-05', profilePhoto: 'https://images.unsplash.com/photo-1558898479-33c0057a5d12?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', photoURL: 'https://images.unsplash.com/photo-1602525962574-3bc829fbed3c?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', comments: [{ user: 'davis_02', comment_text: 'Excelente vista!' }, { user: 'jamal_08', comment_text: 'Llevame!' }, { user: 'petesLocos', comment_text: "AAhh y no llevas man xD" }] },
+  { owner: 'Paula_sofi03', title: 'Enamorados el uno del otro!', createdAt: '2020-12-13 14:33:09.478-05', profilePhoto: 'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', photoURL: 'https://images.unsplash.com/photo-1610141095202-4f36dc1e7899?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=645&q=80', comments: [{ user: 'fulo_003', comment_text: 'Excelente vista!' }] }];
 
   return (
     <MainContainer>
@@ -288,7 +332,7 @@ const Posts = () => {
                 <PostFooterLikes>
                   <button type="button">
                     <span>6</span>
-                    {' '}Likes
+                    &nbsp;Me gusta
                   </button>
                 </PostFooterLikes>
 
@@ -300,6 +344,37 @@ const Posts = () => {
                     &nbsp;{post.title}
                   </span>
                 </PostFooterTitle>
+
+                {
+                  post.comments ?
+                    post.comments.length > 2 ?
+                      <>
+                        <PostFooterSeeComments><Link to="/">Ver los {post.comments.length} comentarios</Link></PostFooterSeeComments>
+                        {post.comments.slice(-2).map((comment, current) => (
+                          <PostFooterComments key={current}>
+                            <span>{comment.user}</span>
+                            <span>&nbsp;{comment.comment_text}</span>
+                          </PostFooterComments>
+                        ))}
+                      </>
+                      :
+                      post.comments.map((comment, current) => (
+                        <PostFooterComments key={current}>
+                          <span>{comment.user}</span>
+                          <span>&nbsp;{comment.comment_text}</span>
+                        </PostFooterComments>
+                      ))
+                    :
+                    null
+                }
+
+                <PostFooterTimer>
+                  <Link to='/'>
+                    <time dateTime={new Date(post.createdAt)} title={format(new Date(post.createdAt), "dd 'de' MMMM 'de' yyyy", { locale: es })}>
+                      Hace {formatDistance(new Date(), new Date(post.createdAt), { locale: es })}
+                    </time>
+                  </Link>
+                </PostFooterTimer>
               </PostFooter>
             </Post>
           ))}
